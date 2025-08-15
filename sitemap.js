a41dcb59f5d2510d29bdc0b83c537889e4151872
calvincyrus9 <calvincyrus9@gmail.com>
2025-08-15 23:53:49 +0100

    15-08-2025

--- sitemap.js
+++ sitemap.js
@@ -11,10 +11,11 @@ const fs = require('fs');
 
     // Find all page files
     const pages = await globby([
-      'pages/**/*.jsx', // Include all .jsx files
-      '!pages/_*.jsx',   // Exclude files like _app.js and _document.js
-      '!pages/api',      // Exclude the api folder
+      'pages/**/*.{js,jsx,ts,tsx}',
+      '!pages/_*.{js,jsx,ts,tsx}',
+      '!pages/api'
     ]);
+
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
